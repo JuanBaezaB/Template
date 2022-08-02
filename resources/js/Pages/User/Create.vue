1fe1dbbfa0b8c1d8ced543c093f8ffd6540f72e0
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2022-08-02 00:26:23 -0400

    Update Create.vue

--- resources/js/Pages/User/Create.vue
+++ resources/js/Pages/User/Create.vue
@@ -2,6 +2,9 @@
     import AppLayout from '@/Layouts/AppLayout.vue';
     import { Inertia } from '@inertiajs/inertia';
     import JetInputError from '@/Jetstream/InputError.vue';
+    import JetInput from '@/Jetstream/Input.vue';
+    import JetLabel from '@/Jetstream/Label.vue';
+    import JetButton from '@/Jetstream/Button.vue';
 </script>
 
 <template>
@@ -15,18 +18,24 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                     <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+                    <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
                         <form @submit.prevent="submit">
-                            <label>Name:</label>
-                            <input type="text" v-model="name" />
-                            <div v-if="errors.name">{{ errors.name }}</div>
-                            <label>Email:</label>
-                            <input type="email" v-model="email" />
-                            <div v-if="errors.email">{{ errors.email }}</div>
-                            <label>Password:</label>
-                            <input type="password"  v-model="password" />
+                            <JetLabel for="name" value="Name" />
+                            <JetInput id="name" v-model="form.name" type="text" class="mt-1 block w-full" autofocus />
+                            <JetInputError :message="errors.name" />
+
+                            <JetLabel for="email" value="Email" />
+                            <JetInput id="email" v-model="form.email" type="email" class="mt-1 block w-full" autofocus />
+                            <JetInputError :message="errors.email" />
+
+                            <JetLabel for="password" value="Password" />
+                            <JetInput id="password" v-model="form.password" type="password" class="mt-1 block w-full"  autofocus />
                             <JetInputError :message="errors.password" />
-                            <button type="submit">Submit</button>
+
+                            <button type="submit"></button>
+                            <JetButton class="mt-1" type="submit">
+                                Submit
+                            </JetButton>
                         </form>
                     </div>
                 </div>
@@ -37,27 +46,28 @@
 
 <script>
 
-    export default {
-        props:["errors"],
-        data() {
-            return {
-                name:'Juan',
-                email:'test@test.cl',
-                password:'1234'
-
+export default {
+    props: ["errors"],
+    data() {
+        return {
+            form: {
+                name: 'Juan',
+                email: 'test@test.cl',
+                password: '1234'
             }
-        },
-        components: {
-            AppLayout
-        },
-        methods: {
-            submit(){
-                Inertia.post(route('user.store'),{
-                    name: this.name,
-                    email: this.email,
-                    password: this.password
-                })
-            }
-        },
-    };
+        }
+    },
+    components: {
+        AppLayout,
+        JetInputError,
+        JetLabel,
+        JetInput,
+        JetButton
+    },
+    methods: {
+        submit() {
+            Inertia.post(route('user.store'), this.form)
+        }
+    },
+};
 </script>
